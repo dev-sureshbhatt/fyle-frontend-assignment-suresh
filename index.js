bfc790a808996d5c57b7fe67d200d3e6b3fcdd6d
dev-sureshbhatt <dev.sureshbhatt@gmail.com>
2024-06-03 11:14:08 +0530

    More work on Dynamic slider using Jquery that changes content on hover. The slider is functional now, need to work on designing.

--- index.js
+++ index.js
@@ -1,27 +1,60 @@
-// // slider jquery
+console.log("hi")
 
-// $(document).ready(function() {
-//     let currentIndex = 0;
-//     const slides = $('.slide');
-//     const dots = $('.dot');
-//     const totalSlides = slides.length;
 
-//     function showSlide(index) {
-//         $('.slider').css('transform', 'translateX(' + (-index * 370) + 'px)');
-//         dots.removeClass('active');
-//         dots.eq(index).addClass('active');
-//     }
 
-//     function nextSlide() {
-//         currentIndex = (currentIndex + 1) % totalSlides;
-//         showSlide(currentIndex);
-//     }
 
-//     dots.click(function() {
-//         currentIndex = $(this).data('index');
-//         showSlide(currentIndex);
-//     });
+$(document).ready(function() {
+    const carouselItems = $('.carousel-item');
+    const carouselData = [{
+        icon: "",
+        title: "Web Development",
+        description: "Web development services",
+        link: "www.fyle.com"
 
-//     showSlide(currentIndex);
-//     setInterval(nextSlide, 2000);
-// });
+    }, {
+        icon: "",
+        title: "Search Engine Optimization",
+        description: "Full Suite SEO services",
+        link: "www.fyle.com"
+
+    }, {
+        icon: "",
+        title: "Search Engine Marketing",
+        description: "Full suite search engine marketing services",
+        link: "www.fyle.com"
+
+    }, {
+        icon: "",
+        title: "Performance Marketing",
+        description: "Pay only for conversion marketing tactics for maximum revenue",
+        link: "www.fyle.com"
+
+    }, {
+        icon: "",
+        title: "Web Security",
+        description: "Web security services at your doorstep",
+        link: "www.fyle.com"
+
+    }];
+
+
+    carouselItems.each(function() {
+        
+        
+        const item = $(this);
+        const itemId = item.attr('id')
+        const fetchIdNumber = itemId.split("-")
+        const index = fetchIdNumber[fetchIdNumber.length - 1]
+        
+        const originalContent = item.html();
+        const newContent = `<span>Icon</span><h1>${carouselData[index-1].title}</h1><p>this is p</p><button>Read More</button>`;
+
+        item.on('mouseenter', function() {
+            item.html(newContent);
+        });
+
+        item.on('mouseleave', function() {
+            item.html(originalContent);
+        });
+    });
+});
